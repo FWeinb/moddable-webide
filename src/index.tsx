f7442e2be19acce11f95c5787e30f3994ef4039d
FWeinb <fabrice@weinberg.me>
2019-06-26 17:03:20 +0200

    Refactor to new DndProvider API

--- src/index.tsx
+++ src/index.tsx
@@ -14,57 +14,57 @@ import Editor from './components/Editor';
 import TopBar from './components/TopBar';
 import ActivityBar from './components/ActivityBar';
 
-import { DragDropContext } from 'react-dnd';
+import { DndProvider } from 'react-dnd';
 import HTML5Backend from './utils/HTML5DirectoryBackend';
-import { Provider } from 'overmind-react';
 
 import { overmind } from './overmind';
+import { Provider as OvermindProvider } from 'overmind-react';
 
 const App: React.FunctionComponent = () => {
   return (
-    <Provider value={overmind}>
-      <div
-        css={{
-          display: 'grid',
-          gridTemplateColumns: '50px auto',
-          gridTemplateRows: '40px auto',
-          height: '100vh'
-        }}
-      >
+    <DndProvider backend={HTML5Backend}>
+      <OvermindProvider value={overmind}>
         <div
           css={{
-            gridRow: 1,
-            gridColumn: '1 / 3',
-            background: 'var(--color-lightest)'
+            display: 'grid',
+            gridTemplateColumns: '50px auto',
+            gridTemplateRows: '40px auto',
+            height: '100vh'
           }}
         >
-          <TopBar />
+          <div
+            css={{
+              gridRow: 1,
+              gridColumn: '1 / 3',
+              background: 'var(--color-lightest)'
+            }}
+          >
+            <TopBar />
+          </div>
+          <div
+            css={{
+              gridRow: 2,
+              gridColumn: 1
+            }}
+          >
+            <ActivityBar />
+          </div>
+          <div
+            css={{
+              gridRow: 2,
+              gridColumn: 2,
+              height: 'calc(100vh - 40px)'
+            }}
+          >
+            <Layout
+              content={<Editor />}
+              sidebar={<SidebarViewContainer />}
+              log={<Log />}
+            />
+          </div>
         </div>
-        <div
-          css={{
-            gridRow: 2,
-            gridColumn: 1
-          }}
-        >
-          <ActivityBar />
-        </div>
-        <div
-          css={{
-            gridRow: 2,
-            gridColumn: 2,
-            height: 'calc(100vh - 40px)'
-          }}
-        >
-          <Layout
-            content={<Editor />}
-            sidebar={<SidebarViewContainer />}
-            log={<Log />}
-          />
-        </div>
-      </div>
-    </Provider>
+      </OvermindProvider>
+    </DndProvider>
   );
 };
-
-const DragDropApp = DragDropContext(HTML5Backend)(App);
-ReactDOM.render(<DragDropApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
